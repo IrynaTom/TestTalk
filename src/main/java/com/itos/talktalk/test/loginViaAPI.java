382b7520dade85732dfddc8e4e22db48fe0d70bd
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-20 15:57:37 +0300

    Make ulr via HttpsURLConnection

--- src/main/java/com/itos/talktalk/test/loginViaAPI.java
+++ src/main/java/com/itos/talktalk/test/loginViaAPI.java
@@ -1,44 +1,44 @@
 package com.itos.talktalk.test;
 
-
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-
 import java.net.*;
 import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
 
 public class loginViaAPI {
 
     public static void main(String[] args) throws Exception {
 
-
-        @Given("I have Construct data")
-        public void сonstructData() {
-
-
-        }
-
         String data = URLEncoder.encode("key1", "UTF-8") + "=" + URLEncoder.encode("value1", "UTF-8");
         data += "&" + URLEncoder.encode("key2", "UTF-8") + "=" + URLEncoder.encode("value2", "UTF-8");
 
         URL apiAuth = new URL("https://localhost:8080/api/v1/auth");
-        URLConnection auth = apiAuth.openConnection();
-        auth.setDoOutput(true);
-        OutputStreamWriter wr = new OutputStreamWriter(auth.getOutputStream());
-        wr.write(data);
-        wr.flush();
+        HttpsURLConnection con = (HttpsURLConnection) apiAuth.openConnection();
 
-        // Get the response
-        BufferedReader in = new BufferedReader(new InputStreamReader(auth.getInputStream()));
-        String inputLine;
+        @When("^I send a POST request to the address$")
+        public void SendPOST() {
+          //  URLConnection auth = apiAuth.openConnection();
+            con.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+            wr.write(data);
+            wr.flush();
+            wr.close();
+        }
 
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-
-        wr.close();
-        in.close();
-
+        @Then("I get a response")
+        public void GetResponse() {
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + apiAuth);
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null)
+                response.append(inputLine);
+            in.close();
+            System.out.println(response.toString());
+        }
     }
 
 }
