0bb42e1abd6b6665a395d0892c84bcc077dda652
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-20 17:34:32 +0300

    Edit LoginViaAPI

--- src/main/java/com/itos/talktalk/test/loginViaAPI.java
+++ src/main/java/com/itos/talktalk/test/loginViaAPI.java
@@ -11,24 +11,28 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class loginViaAPI  {
 
+    URL apiAuth = new URL("https://localhost:8080/api/v1/auth");
+    HttpsURLConnection con = (HttpsURLConnection) apiAuth.openConnection();
+
     public loginViaAPI() throws IOException {
         String data = URLEncoder.encode("key1", "UTF-8") + "=" + URLEncoder.encode("value1", "UTF-8");
         data += "&" + URLEncoder.encode("key2", "UTF-8") + "=" + URLEncoder.encode("value2", "UTF-8");
-        URL apiAuth = new URL("https://localhost:8080/api/v1/auth");
-        HttpsURLConnection con = (HttpsURLConnection) apiAuth.openConnection();
     }
 
+
     @When("^I send a POST request to the address$")
-        public void SendPOST(String data, HttpsURLConnection con)  throws IOException{
+        public void SendPOST(String data)  throws IOException{
           //  URLConnection auth = apiAuth.openConnection();
+
             con.setDoOutput(true);
             OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
             wr.write(data);
             wr.flush();
             wr.close();
         }
+
         @Then("I get a response")
-        public void GetResponse(URL apiAuth,HttpsURLConnection con)  throws IOException{
+        public void GetResponse()  throws IOException{
             int responseCode = con.getResponseCode();
             System.out.println("\nSending 'POST' request to URL : " + apiAuth);
             System.out.println("Response Code : " + responseCode);
